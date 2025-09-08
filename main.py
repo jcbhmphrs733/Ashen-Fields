adc204440cab0fbd94fb41cf56f061f53df08904
jcbhmphrs733 <jacobhumphreys31@gmail.com>
2025-09-08 13:27:50 -0600

    paralax test images added

--- main.py
+++ main.py
@@ -6,10 +6,19 @@ clock = pygame.time.Clock()
 fps = 60
 
 # create game window
-screen_width = 1600
-screen_height = 600
+screen_width = 1200
+screen_height = 500
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Ashen Fields')
+
+bg_images = []
+for i in range(1,5):
+    bg_image = pygame.image.load(f"assets/background/bg-{i}.png").convert_alpha()
+    bg_images.append(bg_image)
+    
+def draw_bg():
+    for i in bg_images:
+        screen.blit(i, (0,0))
 
 # gameloop
 run = True
@@ -17,8 +26,7 @@ while run:
 
     clock.tick(fps)
 
-    # draw background
-    screen.fill((202, 228, 241))
+    draw_bg()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
